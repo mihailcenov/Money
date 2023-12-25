b549f6a6782efbeb3953753ef33124072634bec8
mihailcenov <mihailceov@gmail.com>
2023-12-25 20:23:31 +0200

    Program algorithm is ready

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var bitcoins = double.Parse(Console.ReadLine());
+            var uans = double.Parse(Console.ReadLine());
+            var commission = double.Parse(Console.ReadLine()) / 100.00;
+
+            var bitcoinstoleva = 1168;
+            var uanstodollars = 0.15;
+            var dollarstoleva = 1.76;
+            var levatoeuro = 1.95;
+
+            var levove = bitcoins * bitcoinstoleva;
+            var dollars = uans * uanstodollars;
+            levove += dollarstoleva * dollars;
+
+            var commissionlv = levove * commission;
+            levove -= commissionlv;
+
+            var euro = levatoeuro * levove;
+
+            Console.WriteLine("{0}", euro);
         }
     }
 }
