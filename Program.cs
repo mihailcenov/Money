fba6b9df5fafa00fb1cc58b4ac87e3621b2dc05a
mihailcenov <mihailceov@gmail.com>
2023-12-26 12:22:28 +0200

    Добавена проверка на невалидна комисионна

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ namespace Money
             Console.WriteLine("Комисионна от 0-5%");
             decimal commission = decimal.Parse(Console.ReadLine()) / 100.00m;
 
+            if (commission > (5.0m / 100.00m))
+            {
+                Console.WriteLine("Невалидна комисионна");
+            }
+
             decimal bitcoinstoleva = 1168;
             decimal uanstodollars = 0.15m;
             decimal dollarstoleva = 1.76m;
