3e2e0c09862dbac70a1705246f6f5bc4be495371
mihailcenov <mihailceov@gmail.com>
2023-12-26 12:00:07 +0200

    Corected eror with lev to euro.

--- Program.cs
+++ Program.cs
@@ -1,26 +1,26 @@
-﻿namespace Money
+﻿using System;
+
+namespace Money
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var bitcoins = double.Parse(Console.ReadLine());
-            var uans = double.Parse(Console.ReadLine());
-            var commission = double.Parse(Console.ReadLine()) / 100.00;
+            decimal bitcoins = int.Parse(Console.ReadLine());
+            decimal uans = decimal.Parse(Console.ReadLine());
+            decimal commission = decimal.Parse(Console.ReadLine()) / 100.00m;
 
-            var bitcoinstoleva = 1168;
-            var uanstodollars = 0.15;
-            var dollarstoleva = 1.76;
-            var levatoeuro = 1.95;
+            decimal bitcoinstoleva = 1168;
+            decimal uanstodollars = 0.15m;
+            decimal dollarstoleva = 1.76m;
+            decimal levatoeuro = 1.95m;
 
-            var levove = bitcoins * bitcoinstoleva;
-            var dollars = uans * uanstodollars;
+            decimal levove = bitcoins * bitcoinstoleva;
+            decimal dollars = uans * uanstodollars;
             levove += dollarstoleva * dollars;
 
-            var commissionlv = levove * commission;
-            levove -= commissionlv;
-
-            var euro = levatoeuro * levove;
+            decimal euro = levove / levatoeuro;
+            euro -= commission * euro;
 
             Console.WriteLine("{0}", euro);
         }
